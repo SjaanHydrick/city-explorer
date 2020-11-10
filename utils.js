7554d1a1888127944f3549ff5dccbae6b958645e
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-11-09 17:34:43 -0800

    fixed mungeLocation again

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 function mungeLocation(location) {
   return {
     formatted_query: location[0].display_name,
-    latitude: location[0].latitude,
-    longitude: location[0].longitude
+    latitude: location[0].lat,
+    longitude: location[0].lon
   };
 }
 
