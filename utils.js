c189123ad1446d5124a1549ecb905b7542f1d345
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-11-09 16:41:47 -0800

    weather added

--- utils.js
+++ utils.js
@@ -1,12 +1,22 @@
 function mungeLocation(location) {
-    return {
-      formatted_query: location[0].display_name,
-      latitude: location[0].lat,
-      longitude: location[0].lon
-    };
-  }
-  
-  module.exports = {
-    mungeLocation
+  return {
+    formatted_query: location[0].display_name,
+    latitude: location[0].lat,
+    longitude: location[0].lon
   };
+}
   
+module.exports = {
+  mungeLocation
+};
+
+function mungeWeather(weather) {
+  return {
+    forecast: weather.description,
+    time: weather.datetime,
+  };
+}
+
+module.exports = {
+  mungeWeather
+};
